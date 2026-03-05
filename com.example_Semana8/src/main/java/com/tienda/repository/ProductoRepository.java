7d05ecf30fa3748343f32f0963e64cff924ff05e
ceporras <90878215+ceporras@users.noreply.github.com>
2026-03-05 17:04:24 -0600

    CommitPracticaProgramada2 (#1)
    se crean adv queries para producto y categoria usando derivadas, JPQL y SQL
    
    Co-authored-by: ceporras <ceporrasb@hotmail.com>

--- com.example_Semana8/src/main/java/com/tienda/repository/ProductoRepository.java
+++ com.example_Semana8/src/main/java/com/tienda/repository/ProductoRepository.java
@@ -12,6 +12,8 @@ import org.springframework.data.repository.query.Param;
 public interface ProductoRepository extends JpaRepository<Producto, Integer>{
     public List<Producto> findByActivoTrue();
     
+   
+    
     //ejemplo utilizando consultas derivadas
     public List<Producto> findByPrecioBetweenOrderByPrecioAsc(double precioInf, double precioSup);
     
@@ -23,5 +25,19 @@ public interface ProductoRepository extends JpaRepository<Producto, Integer>{
     @Query(nativeQuery = true, value = "SELECT * FROM producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio ASC")
     public List<Producto> consultaSQL(@Param("precioInf") double precioInf,@Param("precioSup") double precioSup);
     
+    //consultas practica programada
+    //
+    //consultas derivadas
+    public List<Producto> findByActivoTrueAndPrecioBetweenAndExistenciasGreaterThanAndCategoriaActivoTrueAndCategoriaDescripcionContainingOrderByPrecioAsc(double precioInf, double precioSup, int minExistencias, String descFiltro);
+    
+    //JPQL
+    @Query(value = "SELECT p FROM Producto p WHERE p.activo=true AND p.precio BETWEEN :precioInf AND :precioSup AND p.existencias > :minExistencias AND p.categoria.activo=true AND p.categoria.descripcion LIKE %:descFiltro% ORDER BY p.precio ASC")
+    public List<Producto> consultaAvanzadaJPQL(@Param("precioInf") double precioInf,@Param("precioSup") double precioSup,@Param("minExistencias") int minExistencias,@Param("descFiltro") String descFiltro);
+    
+    //SQL
+    @Query(nativeQuery = true, value = "SELECT p.* FROM producto p INNER JOIN categoria c ON p.id_categoria = c.id_categoria "
+            + "WHERE p.activo=true AND p.precio BETWEEN :precioInf AND :precioSup AND p.existencias > :minExistencias "
+            + "AND c.activo=true AND c.descripcion LIKE %:descFiltro% ORDER BY p.precio ASC")
+    public List<Producto> consultaAvanzadaSQL(@Param("precioInf") double precioInf,@Param("precioSup") double precioSup,@Param("minExistencias") int minExistencias,@Param("descFiltro") String descFiltro);
     
 }
